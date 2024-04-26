70fd7cc989c7b90a233385d9f49ce9cf2c529cd9
kurtisjw <165825832+kurtisjw@users.noreply.github.com>
2024-04-26 13:35:34 +0200

    Primera Parte

--- your-code/main_esp.py
+++ your-code/main_esp.py
@@ -1,61 +1,84 @@
 #1. Importa el paquete NUMPY bajo el nombre np.
 
 #[tu código aquí]
+import numpy as np
 
 
 #2. Imprime la versión de NUMPY y la configuración.
 
 #[tu código aquí]
-
+print(np.version.version)
+"""
+1.15.2
+"""
 
 #3. Genera un array tridimensional de 2x3x5 con valores aleatorios. Asigna el array a la variable "a"
 # Desafío: hay al menos tres maneras fáciles que usan numpy para generar arrays aleatorios. ¿Cuántas formas puedes encontrar?
 
 #[tu código aquí]
+a = np.random.randint(10, size = (2, 3, 5))
 
 #4. Imprime a.
 
 #[tu código aquí]
+print(a)
+
 #5. Crea un array tridimensional de 5x2x3 con todos los valores igual a 1.
 #Asigna el array a la variable "b"
 
 #[tu código aquí]
+b = np.ones((5, 2, 3))
 
 #6. Imprime b.
 
 #[tu código aquí]
+print(b)
 
 #7. ¿Tienen a y b el mismo tamaño? ¿Cómo lo demuestras en código Python?
 
 #[tu código aquí]
+equal_result = (a == b)
+print(equal_result)
 
 #8. ¿Es posible sumar a y b? ¿Por qué sí o por qué no?
 
 #[tu código aquí]
+# No es posible sumar arrays de diferentes formas.
 
 
 #9. Transpone b para que tenga la misma estructura que a (es decir, se convierta en un array de 2x3x5). Asigna el array transpuesto a la variable "c".
 
 #[tu código aquí]
+#Transponer b para crear c
+c = np.transpose(b, (1, 2, 0)) #Esto cambia el orden de la dimensiones de b - index 1, 2, 0 = 5, 2, 3
+print(c)
 
 #10. Intenta sumar a y c. Ahora debería funcionar. Asigna la suma a la variable "d". Pero, ¿por qué funciona ahora?
 
 #[tu código aquí]
+# Ahora funciona porque hemos transpuesto b (asignado a c) para que tenga la misma forma que a
+d = a + c
+print (d)
 
 #11. Imprime a y d. ¿Notas la diferencia y la relación entre los dos arrays en términos de los valores? Explica.
 
 #[tu código aquí]
-
+print(f"This is array a \n {a}")
+print(f"This is array d \n {d}")
+# La diferencia es que los números en d son decimales. Esto es porque b, transpuesto en c, fue creado usando np.ones() y por eso es un array de floating points.
 
 #12. Multiplica a y c. Asigna el resultado a e.
 
 #[tu código aquí]
+e = a * c
 
 
 #13. ¿Es e igual a a? ¿Por qué sí o por qué no?
 
 #[tu código aquí]
-
+e_a_equal = (e == a)
+print(e_a_equal)
+# False. Parece que son iguales aparte del hecho que los números en e son decimales/floating points. 
 
 
 #14. Identifica los valores máximos, mínimos y medios en d. Asigna esos valores a las variables "d_max", "d_min" y "d_mean"
